bcb6a99df3f3a21ab3c5f22666b5b5f1a3af3d73
Abay <temirbolatov17@gmail.com>
2022-05-21 23:20:27 +0600

    fixed remembermyfilms function

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ const personalMovieDB = {
     },
     rememberMyFilms: function() {
         for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последних просмотренных фильмов?', ''),
-                  b = prompt('На сколько оцените его?', '');
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt('На сколько оцените его?', '').trim();
             
             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
                 personalMovieDB.movies[a] = b;
